466e96489224aff859ad3d1d0e887325552a70e8
gyorgy-forwardvaping <gyorgy@forwardvaping.com>
2023-06-14 11:32:19 +0100

    one to one relationship crud

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Address;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert', function () {
+    $user = User::findOrFail(1);
+
+    $address = new Address(['name' => 'bham woodland farm road 3B']);
+    $user->address()->save($address);
+});
+
+Route::get('/update', function () {
+    // $address = Address::find(1);
+    // return $address->user;
+
+    $address = Address::whereUserId(1)->first();
+
+    $address->name = "updated address b240pj";
+    $address->save();
+});
+
+Route::get('/show', function () {
+    $user = User::findOrFail(1);
+    return $user->name . ' live under ' . $user->address->name;
+});
+
+Route::get('/delete', function () {
+    $user = User::findOrFail(1);
+    $user->address()->delete();
+    $user->delete();
 });
